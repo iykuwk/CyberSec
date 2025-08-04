b5ef38cf29f2e21276603c3dc13f75eee40313f8
iykuwk <7yashzingade@gmail.com>
2025-08-04 13:23:43 +0530

    IPv^ obfuscation

---
+++ IPv6Obfuscation/main.c
@@ -0,0 +1,138 @@
+#include <Windows.h>
+#include <stdio.h>
+
+// disable error 4996 (caused by sprint)
+#pragma warning (disable:4996)
+
+// Function takes in 16 raw bytes and returns them in an IPv6 address string format
+// 16*8=128 bits where 8 bits & 16 bytes are considered
+char* GenerateIpv6(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p) {
+
+	// Each IPv6 segment is 32 bytes
+	char Output0[32], Output1[32], Output2[32], Output3[32];
+
+	// There are 4 segments in an IPv6 (32 * 4 = 128)
+	char result[128];
+
+	// Generating output0 using the first 4 bytes
+	sprintf(Output0, "%0.2X%0.2X:%0.2X%0.2X", a, b, c, d);
+
+	// Generating output1 using the second 4 bytes
+	sprintf(Output1, "%0.2X%0.2X:%0.2X%0.2X", e, f, g, h);
+
+	// Generating output2 using the third 4 bytes
+	sprintf(Output2, "%0.2X%0.2X:%0.2X%0.2X", i, j, k, l);
+
+	// Generating output3 using the last 4 bytes
+	sprintf(Output3, "%0.2X%0.2X:%0.2X%0.2X", m, n, o, p);
+
+	// Combining Output0,1,2,3 to generate the IPv6 address
+	sprintf(result, "%s:%s:%s:%s", Output0, Output1, Output2, Output3);
+
+	// Optional: Print the 'result' variable to the console
+	// printf("[i] result: %s\n", (char*)result);
+
+	return (char*)result;
+}
+
+// Generate the IPv6 output representation of the shellcode
+// Function requires a pointer or base address to the shellcode buffer & the size of the shellcode buffer
+BOOL GenerateIpv6Output(unsigned char* pShellcode, SIZE_T ShellcodeSize) {
+	// If the shellcode buffer is null or the size is not a multiple of 16, exit
+	if (pShellcode == NULL || ShellcodeSize == NULL || ShellcodeSize % 16 != 0){
+		return FALSE;
+	}
+	printf("char* Ipv6Array [%d] = { \n\t", (int)(ShellcodeSize / 16));
+	
+	// We will read one shellcode byte at a time, when the total is 16, begin generating the IPv6 address
+	// The variable 'c' is used to store the number of bytes read. By default, starts at 16.
+	int c = 16, counter = 0;
+	char* IP = NULL;
+	
+	for (int i = 0; i < ShellcodeSize; i++) {
+		// Track the number of bytes read and when they reach 16 we enter this if statement to begin generating the IPv6 address
+		if (c == 16) {
+			counter++;
+
+			// Generating the IPv6 address from 16 bytes which begin at i until [i + 15]
+			IP = GenerateIpv6(
+				pShellcode[i], pShellcode[i + 1], pShellcode[i + 2], pShellcode[i + 3],
+				pShellcode[i + 4], pShellcode[i + 5], pShellcode[i + 6], pShellcode[i + 7],
+				pShellcode[i + 8], pShellcode[i + 9], pShellcode[i + 10], pShellcode[i + 11],
+				pShellcode[i + 12], pShellcode[i + 13], pShellcode[i + 14], pShellcode[i + 15]
+			);
+			if (i == ShellcodeSize - 16) {
+
+				// Printing the last IPv6 address
+				printf("\"%s\",\n", IP);
+				break;
+			}
+			else {
+				// Printing the IPv6 address
+				printf("\"%s\", \n", IP);
+			}
+			c = 1;
+
+			// Optional: To beautify the output on the console
+			if (counter % 3 == 0) {
+				printf("\n\t");
+			}
+		}
+		else {
+			c++;
+		}
+	}
+	printf("\n};\n\n");
+	return TRUE;
+}
+
+// x64 calc Msfvenom shellcode {272 bytes}
+unsigned char rawData[] = {
+	0xFC, 0x48, 0x83, 0xE4, 0xF0, 0xE8, 0xC0, 0x00, 0x00, 0x00, 0x41, 0x51,
+	0x41, 0x50, 0x52, 0x51, 0x56, 0x48, 0x31, 0xD2, 0x65, 0x48, 0x8B, 0x52,
+	0x60, 0x48, 0x8B, 0x52, 0x18, 0x48, 0x8B, 0x52, 0x20, 0x48, 0x8B, 0x72,
+	0x50, 0x48, 0x0F, 0xB7, 0x4A, 0x4A, 0x4D, 0x31, 0xC9, 0x48, 0x31, 0xC0,
+	0xAC, 0x3C, 0x61, 0x7C, 0x02, 0x2C, 0x20, 0x41, 0xC1, 0xC9, 0x0D, 0x41,
+	0x01, 0xC1, 0xE2, 0xED, 0x52, 0x41, 0x51, 0x48, 0x8B, 0x52, 0x20, 0x8B,
+	0x42, 0x3C, 0x48, 0x01, 0xD0, 0x8B, 0x80, 0x88, 0x00, 0x00, 0x00, 0x48,
+	0x85, 0xC0, 0x74, 0x67, 0x48, 0x01, 0xD0, 0x50, 0x8B, 0x48, 0x18, 0x44,
+	0x8B, 0x40, 0x20, 0x49, 0x01, 0xD0, 0xE3, 0x56, 0x48, 0xFF, 0xC9, 0x41,
+	0x8B, 0x34, 0x88, 0x48, 0x01, 0xD6, 0x4D, 0x31, 0xC9, 0x48, 0x31, 0xC0,
+	0xAC, 0x41, 0xC1, 0xC9, 0x0D, 0x41, 0x01, 0xC1, 0x38, 0xE0, 0x75, 0xF1,
+	0x4C, 0x03, 0x4C, 0x24, 0x08, 0x45, 0x39, 0xD1, 0x75, 0xD8, 0x58, 0x44,
+	0x8B, 0x40, 0x24, 0x49, 0x01, 0xD0, 0x66, 0x41, 0x8B, 0x0C, 0x48, 0x44,
+	0x8B, 0x40, 0x1C, 0x49, 0x01, 0xD0, 0x41, 0x8B, 0x04, 0x88, 0x48, 0x01,
+	0xD0, 0x41, 0x58, 0x41, 0x58, 0x5E, 0x59, 0x5A, 0x41, 0x58, 0x41, 0x59,
+	0x41, 0x5A, 0x48, 0x83, 0xEC, 0x20, 0x41, 0x52, 0xFF, 0xE0, 0x58, 0x41,
+	0x59, 0x5A, 0x48, 0x8B, 0x12, 0xE9, 0x57, 0xFF, 0xFF, 0xFF, 0x5D, 0x48,
+	0xBA, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0x8D, 0x8D,
+	0x01, 0x01, 0x00, 0x00, 0x41, 0xBA, 0x31, 0x8B, 0x6F, 0x87, 0xFF, 0xD5,
+	0xBB, 0xE0, 0x1D, 0x2A, 0x0A, 0x41, 0xBA, 0xA6, 0x95, 0xBD, 0x9D, 0xFF,
+	0xD5, 0x48, 0x83, 0xC4, 0x28, 0x3C, 0x06, 0x7C, 0x0A, 0x80, 0xFB, 0xE0,
+	0x75, 0x05, 0xBB, 0x47, 0x13, 0x72, 0x6F, 0x6A, 0x00, 0x59, 0x41, 0x89,
+	0xDA, 0xFF, 0xD5, 0x63, 0x61, 0x6C, 0x63, 0x00
+};
+
+int main() {
+
+	if (!GenerateIpv6Output(rawData, sizeof(rawData))) {
+		// if failed, that is sizeof(rawData) isnt multiple of 16
+		return -1;
+	}
+
+	printf("[#] Press <Enter> To Quit ... ");
+	getchar();
+	return 0;
+}
+
+/*
+output:
+char* Ipv6Array [17] = {
+		"FC48:83E4:F0E8:C000:0000:4151:4150:5251", "5648:31D2:6548:8B52:6048:8B52:1848:8B52", "2048:8B72:5048:0FB7:4A4A:4D31:C948:31C0",
+		"AC3C:617C:022C:2041:C1C9:0D41:01C1:E2ED", "5241:5148:8B52:208B:423C:4801:D08B:8088", "0000:0048:85C0:7467:4801:D050:8B48:1844",
+		"8B40:2049:01D0:E356:48FF:C941:8B34:8848", "01D6:4D31:C948:31C0:AC41:C1C9:0D41:01C1", "38E0:75F1:4C03:4C24:0845:39D1:75D8:5844",
+		"8B40:2449:01D0:6641:8B0C:4844:8B40:1C49", "01D0:418B:0488:4801:D041:5841:585E:595A", "4158:4159:415A:4883:EC20:4152:FFE0:5841",
+		"595A:488B:12E9:57FF:FFFF:5D48:BA01:0000", "0000:0000:0048:8D8D:0101:0000:41BA:318B", "6F87:FFD5:BBE0:1D2A:0A41:BAA6:95BD:9DFF",
+		"D548:83C4:283C:067C:0A80:FBE0:7505:BB47", "1372:6F6A:0059:4189:DAFF:D563:616C:6300"
+};
+*/
